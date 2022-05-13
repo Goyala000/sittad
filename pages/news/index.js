219aa5e57a2eabf2fb10a455e7326e5c63be2bd9
Ankit Goyala <ankitgoyala2@gmail.com>
2022-05-13 18:41:46 +0545

    last

--- pages/news/index.js
+++ pages/news/index.js
@@ -5,9 +5,8 @@ import Layout from "@/components/Layout";
 
 import styles from "@/styles/News.module.css";
 import { API_URL } from "@/config/index";
-import fetchNews from "fetch";
 
-export default function NewsPage() {
+export default function NewsPage({ news }) {
   var options = {
     weekday: "long",
     year: "numeric",
@@ -19,56 +18,40 @@ export default function NewsPage() {
       <ImageHeader mtitle="News" />
 
       <Container>
-        <div className={styles.title}>Latest News News</div>
+        <div className={styles.title}>Latest News</div>
 
         <Row xs={1} md={2} className="g-4 container my-5 mx-2">
-          {news &&
-            news.map((n) => (
-              <Col key={n._id}>
-                <Card>
-                  <Card.Img variant="top" src={`${n.image}`} />
-                  <Card.Body>
-                    <p>
-                      {new Date(n.createdAt).toLocaleDateString(
-                        "en-US",
-                        options
-                      )}
-                    </p>
-                    <Link href={`/news/${n._id}`}>
-                      <Card.Title>{n.title}</Card.Title>
-                    </Link>
-                    <Card.Text>
-                      {" "}
-                      {n.description && n.description.substring(0, 299)}.......
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-            ))}
+          {news.map((n) => (
+            <Col key={n._id}>
+              <Card>
+                <Card.Img variant="top" src={`${n.image}`} />
+                <Card.Body>
+                  <p>
+                    {new Date(n.createdAt).toLocaleDateString("en-US", options)}
+                  </p>
+                  <Link href={`/news/${n._id}`}>
+                    <Card.Title>{n.title}</Card.Title>
+                  </Link>
+                  <Card.Text>
+                    {" "}
+                    {n.description && n.description.substring(0, 299)}.......
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
         </Row>
       </Container>
     </Layout>
   );
 }
 
-export async function getServerSideProps() {
-  // const res = await fetch(`/api/news`);
-  // const news = await res.json();
-
-  const data = await fetchNews();
-  const news = await data.json();
-
-  if (!news) {
-    return {
-      redirect: {
-        destination: "/",
-        permanent: false,
-        // statusCode: 301
-      },
-    };
-  }
+export async function getStaticProps() {
+  const res = await fetch(`${API_URL}/api/news`);
+  const news = await res.json();
 
   return {
     props: { news },
+    revalidate: 1,
   };
 }
